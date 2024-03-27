847551a6894294940f26cf8948ffc24d718a8af3
ramonzxc1 <ramonalternativeacc@gmail.com>
2024-03-27 14:29:23 -0300

    getters and setters added

--- src/main/java/br/ufrn/imd/model/Deck.java
+++ src/main/java/br/ufrn/imd/model/Deck.java
@@ -2,4 +2,12 @@ package br.ufrn.imd.model;
 
 public class Deck {
     private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
