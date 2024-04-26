947f2016ebd5a01ea68f6fa79c7f8e8d7f8a3397
daniel <danielnobre09@gmail.com>
2024-04-26 17:10:02 -0300

    classe Deck, refatorar interações com player depois e conectar com banco de dados

---
+++ src/main/java/br/ufrn/imd/model/Deck.java
@@ -0,0 +1,50 @@
+package br.ufrn.imd.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Map;
+
+@Document(collection = "decks")
+public class Deck {
+    @Id
+    private String id;
+
+    @Field("deck_name")
+    private String deckName;
+
+    @Field("position_frequencies")
+    private Map<Integer, Integer> positionFrequencies;
+
+    // Construtores, getters e setters
+
+    public Deck() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDeckName() {
+        return deckName;
+    }
+
+    public void setDeckName(String deckName) {
+        this.deckName = deckName;
+    }
+
+    public Map<Integer, Integer> getPositionFrequencies() {
+        return positionFrequencies;
+    }
+
+    public void setPositionFrequencies(Map<Integer, Integer> positionFrequencies) {
+        this.positionFrequencies = positionFrequencies;
+    }
+
+    // toString, equals, hashCode
+}
