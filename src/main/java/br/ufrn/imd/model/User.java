fee6e13cec38ef7c7d986a613115072bf6fda650
daniel <danielnobre09@gmail.com>
2024-04-30 02:03:51 -0300

    Roles

--- src/main/java/br/ufrn/imd/model/User.java
+++ src/main/java/br/ufrn/imd/model/User.java
@@ -15,6 +15,7 @@ public class User {
     private String username;
     private String email;
     private String password;
+    private String role;
 
     /**
      * Constructs a new User with provided credentials.
@@ -72,4 +73,12 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
